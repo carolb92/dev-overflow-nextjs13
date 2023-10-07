d2957684080fd6ae78de4b2bbf670e85df0f0981
Carol B <carolb@bu.edu>
2023-10-06 22:39:59 -0400

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,28 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next"; // this is so typescript knows we're working with metadata
+import "./globals.css";
 
-export const metadata = {
-	title: "Next.js 13 with Clerk",
+const inter = Inter({
+	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+	subsets: ["latin"],
+	weight: ["300", "400", "500", "600", "700"],
+	variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+	title: "DevFlow",
+	description:
+		"A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+	icons: {
+		icon: "/assets/images/site-logo.svg",
+	},
 };
 
 export default function RootLayout({
@@ -11,9 +31,19 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<ClerkProvider>
+		<ClerkProvider
+			appearance={{
+				elements: {
+					formButtonPrimary: "primary-gradient",
+					footerActionLink: "primary-gradient hover:text-primary-500",
+				},
+			}}
+		>
 			<html lang="en">
-				<body>{children}</body>
+				<body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+					<h1 className="h1-bold">H1 text</h1>
+					{children}
+				</body>
 			</html>
 		</ClerkProvider>
 	);
